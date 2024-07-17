a8c2dece346c2c3865d1902e1c14fa80959b8d86
Mayur Patil <113410133+PVmayur@users.noreply.github.com>
2024-07-18 00:16:20 +0530

    Update script.js

--- MVP/script.js
+++ MVP/script.js
@@ -97,3 +97,33 @@ function showAllImages(category) {
         imageDisplay.appendChild(container);
     });
 }
+
+
+// 
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     breakpoints: {
+//         640: {
+//             slidesPerView: 2,
+//             spaceBetween: 10,
+//         },
+//         768: {
+//             slidesPerView: 3,
+//             spaceBetween: 15,
+//         },
+//         1024: {
+//             slidesPerView: 4,
+//             spaceBetween: 20,
+//         },
+//         1280: {
+//             slidesPerView: 5,
+//             spaceBetween: 25,
+//         },
+//     },
+// });
+// 
